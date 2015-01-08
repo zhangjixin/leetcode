5ac12b3a693a2892bf8284001e1b21742c4d5283
zhangjixin <zhangjixin@users.noreply.github.com>
2015-01-08 15:36:40 +0800

    Update merge-two-sorted-lists.cpp

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -11,18 +11,24 @@ public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         ListNode head(0);
         ListNode *p = &head;
-        while(l1 != NULL && l2 != NULL) {
-            if(l1->val < l2->val) {
+        while (l1 != NULL || l2 != NULL) {
+            if (l1 == NULL) {
+                p->next = l2;
+                break;
+            }
+            if (l2 == NULL) {
+                p->next = l1;
+                break;
+            }
+            if (l1->val < l2->val) {
                 p->next = l1;
                 l1 = l1->next;
-            }else{
+            } else {
                 p->next = l2;
                 l2 = l2->next;
             }
             p = p->next;
         }
-        if(l1 != NULL ) p->next = l1;
-        if(l2 != NULL) p->next = l2;
         return head.next;
     }
 };
