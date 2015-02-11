605fe0162f21124972e8a3400592c62a9dc67b7d
zhangjixin <jxin_zhang@163.com>
2015-02-11 22:01:31 +0800

    no problem

---
+++ same-tree.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (p == NULL) {
+            if (q == NULL) return true;
+            return false;
+        }
+        if (q != NULL && p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        return false;
+    }
+};
