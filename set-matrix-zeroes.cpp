d6032d0ce1e1de86513e7caa7837aeaf1a9a2b79
zhangjixin <jxin_zhang@163.com>
2015-02-12 21:34:30 +0800

    multi review

--- set-matrix-zeroes.cpp
+++ set-matrix-zeroes.cpp
@@ -29,3 +29,40 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int> > &matrix) {
+        bool row0(false), col0(false);
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (!matrix[i][j]) {
+                    if (!i) row0 = true;
+                    if (!j) col0 = true;
+                    matrix[i][0] = 0, matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; ++i) {
+            if (!matrix[i][0]) {
+                int j = 1;
+                while (j < n) matrix[i][j++] = 0;
+            }
+        }
+        for (int i = 1; i < n; ++i) {
+            if (!matrix[0][i]) {
+                int j = 1;
+                while (j < m) matrix[j++][i] = 0;
+            }
+        }
+        if (row0) {
+            int i = 0;
+            while (i < n) matrix[0][i++] = 0;
+        }
+        if (col0) {
+            int i = 0;
+            while (i < m) matrix[i++][0] = 0;
+        }
+    }
+};
