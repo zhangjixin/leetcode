f58a926eaff64c8b0378e30e81a15ac2493ff106
zhangjixin <jxin_zhang@163.com>
2015-05-21 20:53:43 +0800

    review

--- bitwise-and-of-numbers-range.cpp
+++ bitwise-and-of-numbers-range.cpp
@@ -14,3 +14,16 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int bit = 0;
+        while (m != n) {
+            m >>= 1;
+            n >>= 1;
+            ++bit;
+        }
+        return m << bit;
+    }
+};
